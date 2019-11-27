f4492cde240fe01b30e0df5f10e5a91d279ead63
enrisan <easanchez@gmail.com>
2019-11-26 21:58:11 -0600

    format correction with PEP 8 online.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 agenda_telefonica = dict()
 
-
 def imprimir_operacion(nombre_operacion):
     print()
     print("-------- Agenda telefónica --------")
@@ -15,13 +14,13 @@ def agregar_contacto():
     contacto = input("Nombre del nuevo contacto: ")
     numero = input("Número del nuevo contacto: ")
     agenda_telefonica[contacto] = numero
-  
+
     imprimir_operacion(contacto + " agregado con el número " + numero + ".")
 
 
 def remover_contacto():
     contacto = input("Nombre del contacto que deseas borrar: ")
-    try: 
+    try:
         del agenda_telefonica[contacto]
     except KeyError:
         imprimir_operacion(contacto + " no existe.")
@@ -35,14 +34,16 @@ def actualizar_contacto():
 
     agenda_telefonica[contacto] = numero
     imprimir_operacion(contacto + " actualizado a: " + numero)
-  
+
+
 def ver_contacto():
     contacto = input("Nombre del contacto: ")
     try:
-        imprimir_operacion("{} - {}".format(contacto,agenda_telefonica[contacto]))
+        imprimir_operacion("{} - {}".format(contacto, agenda_telefonica[contacto]))
     except KeyError:
         imprimir_operacion(contacto + " no existe.")
-  
+
+
 def ver_todo():
     nombre_operacion = None
 
@@ -51,44 +52,43 @@ def ver_todo():
     else:
         for contacto in agenda_telefonica:
             if nombre_operacion == None:
-                nombre_operacion = "{} - {}".format(contacto,agenda_telefonica[contacto])
+                nombre_operacion = "{} - {}".format(contacto, agenda_telefonica[contacto])
             else:
-                nombre_operacion += "\n{} - {}".format(contacto,agenda_telefonica[contacto])
+                nombre_operacion += "\n{} - {}".format(contacto, agenda_telefonica[contacto])
 
     imprimir_operacion(nombre_operacion)
 
 print("**** Bienvenido a la Agenda Telefónica de Enrique ****")
 
 while True:
-  print("1 - Agregar un contacto")
-  print("2 - Remover un contacto")
-  print("3 - Actualizar un contacto")
-  print("4 - Ver un contacto")
-  print("5 - Ver todos los contactos")
-  print("6 - Salir")
-  print()
-
-  try:
-    operacion = int(input(": "))
-  except ValueError:
+    print("1 - Agregar un contacto")
+    print("2 - Remover un contacto")
+    print("3 - Actualizar un contacto")
+    print("4 - Ver un contacto")
+    print("5 - Ver todos los contactos")
+    print("6 - Salir")
     print()
-    print ("Selecciona un número del 1 al 6.")
-    print()
-  else:   
-    if operacion == 1:
-      agregar_contacto()
-    elif operacion == 2:
-      remover_contacto()
-    elif operacion == 3:
-      actualizar_contacto()
-    elif operacion == 4:
-      ver_contacto()
-    elif operacion == 5:
-      ver_todo()
-    elif operacion == 6:
-      break
+    try:
+        operacion = int(input(": "))
+    except ValueError:
+        print()
+        print ("Selecciona un número del 1 al 6.")
+        print()
     else:
-      print ("Operación desconocida")
+        if operacion == 1:
+            agregar_contacto()
+        elif operacion == 2:
+            remover_contacto()
+        elif operacion == 3:
+            actualizar_contacto()
+        elif operacion == 4:
+            ver_contacto()
+        elif operacion == 5:
+            ver_todo()
+        elif operacion == 6:
+            break
+        else:
+            print ("Operación desconocida")
 
 print()
 print("Gracias por usar nuestra Agenda Telefónica")
